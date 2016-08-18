d3fbd5e955dcf2193b3bc650bc32e92d04f4ed2e
Harish Narayanan <hnarayanan@gmail.com>
2016-08-18 22:26:32 +0100

    Simplify code by using the generic create view for uploading photos

--- photos/urls.py
+++ photos/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import include, url
 
-from .views import PhotoList, PhotoDetail, PhotoUpload
+from .views import PhotoCreate, PhotoDetail, PhotoList
 
 
 urlpatterns = [
-    url(r'^$', PhotoList.as_view(), name='list'),
+    url(r'^upload/$', PhotoCreate.as_view(), name='upload'),
     url(r'^(?P<pk>[0-9]+)/$', PhotoDetail.as_view(), name='detail'),
-    url(r'^upload/$', PhotoUpload.as_view(), name='upload'),
+    url(r'^$', PhotoList.as_view(), name='list'),
 ]
--- photos/views.py
+++ photos/views.py
@@ -1,15 +1,12 @@
-from django.views.generic import DetailView, ListView
-from django.views.generic.edit import FormView
-from django.core.urlresolvers import reverse
+from django.views.generic import CreateView, DetailView, ListView
 
 from .models import Photo
-from .forms import PhotoUploadForm
 
 
-class PhotoList(ListView):
+class PhotoCreate(CreateView):
 
     model = Photo
-    context_object_name = 'photos'
+    fields = ['image', 'title', 'style']
 
 
 class PhotoDetail(DetailView):
@@ -18,20 +15,9 @@ class PhotoDetail(DetailView):
     context_object_name = 'photo'
 
 
-class PhotoUpload(FormView):
+class PhotoList(ListView):
 
-    template_name = 'photos/photo_upload.html'
-    form_class = PhotoUploadForm
+    model = Photo
+    context_object_name = 'photos'
 
-    def form_valid(self, form):
-        photo = Photo(
-            image=self.get_form_kwargs().get('files')['image'],
-            title=self.get_form_kwargs().get('data')['title'],
-            style=self.get_form_kwargs().get('data')['style'],
-        )
-        photo.save()
-        self.photo_id = photo.id
-        return super(PhotoUpload, self).form_valid(self)
 
-    def get_success_url(self):
-        return reverse('photo:detail', kwargs={'pk': self.photo_id})
