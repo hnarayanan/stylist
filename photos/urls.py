64ef89a95191d84399dc0f31f1716cf4143ad950
Harish Narayanan <hnarayanan@gmail.com>
2016-11-06 22:29:05 +0000

    Make the root URL the home page

--- photos/urls.py
+++ photos/urls.py
@@ -1,10 +1,9 @@
 from django.conf.urls import include, url
 
-from .views import PhotoCreate, PhotoDetail, PhotoList
+from .views import PhotoCreate, PhotoDetail
 
 
 urlpatterns = [
     url(r'^upload/$', PhotoCreate.as_view(), name='upload'),
     url(r'^(?P<pk>[0-9]+)/$', PhotoDetail.as_view(), name='detail'),
-    url(r'^$', PhotoList.as_view(), name='list'),
 ]
--- stylist/urls.py
+++ stylist/urls.py
@@ -3,8 +3,11 @@ from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.contrib import admin
 
+from photos.views import PhotoList
+
 
 urlpatterns = [
+    url(r'^$', PhotoList.as_view(), name='home'),
+    url(r'^photos/', include('photos.urls', namespace='photo')),
     url(r'^admin/', admin.site.urls),
-    url(r'^photos/', include('photos.urls', namespace='photo'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
